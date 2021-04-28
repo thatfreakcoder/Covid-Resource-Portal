a8137322c3169ce62d5264e3def5891d05644c09
Yuvraj Dagur <yuvraj.dagur928@gmail.com>
2021-04-29 01:50:16 +0530

    create search routes for location and tags

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3.6
 
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, redirect, render_template, flash, url_for
 from flask_mysqldb import MySQL
-from os import urandom
 from yaml import load, FullLoader
+from datetime import datetime 
 
 app = Flask(__name__)
 mysql = MySQL(app)
@@ -14,7 +14,6 @@ app.config['MYSQL_HOST'] = db_keeps['mysql_host']
 app.config['MYSQL_USER'] = db_keeps['mysql_user']
 app.config['MYSQL_PASSWORD'] = db_keeps['mysql_password']
 app.config['MYSQL_DB'] = db_keeps['mysql_db']
-app.config['SECRET_KEY'] = urandom(24)
 
 @app.route('/')
 def index():
@@ -22,11 +21,51 @@ def index():
     q = cur.execute("SELECT * FROM resources ORDER BY res_id DESC;")
     if q > 0:
         resources = cur.fetchall()
-        print(resources)
         return render_template('index.html', resources=resources)
     else:
         return render_template('index.html', resources=None)
 
+@app.route('/new/')
+def post():
+    if request.method == 'POST':
+        result = request.form
+        description = str(result['description'])
+        datetime = str(datetime.today().strftime("%Y-%m-%d %H:%M:%S"))
+        if result['user']: 
+            user = result['user']
+        else:
+            user = None
+        tags = str(request.form.getlist('tags'))[1:-1]
+        location = str(result['location']).lower()
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO resources(description, datetime, user, tags, location) VALUES(%s, %s, %s, %s, %s);", (description, datetime, user, tags, location))
+        mysql.connection.commit()
+        cur.close()
+        flash("Thank You for your contribution.", "success")
+        return redirect('/')
+    return render_template('new.html')
+
+@app.route('/search/locations')
+def search_location():
+    location = str(request.args.get('q')).lower()
+    cur = mysql.connection.cursor()
+    q = cur.execute("SELECT * FROM resources WHERE location='{}';".format(location))
+    if q > 0:
+        resources = cur.fetchall()
+        return render_template('index.html', resources=resources)
+    else:
+        return render_template('index.html', resources=None)
+
+@app.route('/search/tags')
+def search_tag():
+    tag = str(request.args.get('q')).upper()
+    cur = mysql.connection.cursor()
+    q = cur.execute("SELECT * FROM resources WHERE tags LIKE '%{}%';".format(tag))
+    if q > 0:
+        resources = cur.fetchall()
+        return render_template('index.html', resources=resources)
+    else:
+        return render_template('index.html', resources=None)
 
 if __name__ == '__main__':
     app.run(debug=True)
